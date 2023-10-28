88fc15d5a1a8564f7acb6809715762ad5ce714fd
Lucas Perrone <lucaspramos21@gmail.com>
2023-10-28 20:57:32 -0300

    Quase finalização da home

---
+++ template.js
@@ -0,0 +1,32 @@
+function geraTemplate(
+    capaEndereco,
+    categoria,
+    tituloLivro,
+    autorLivro,
+    idiomaLivro,
+    dataPublicacao,
+    editoraLivro
+) {
+    let template = `<li class="container-livro-item">
+<div>
+    <img class="imagem-livro" src="${capaEndereco}" alt="">
+</div>
+<div>
+    <div class="livros-dados">
+        <p class="livro-categoria">Categoria: ${categoria}</p>
+        <h3 class="livro-titulo">${tituloLivro}</h3>
+        <p class="livro-autor">${autorLivro}</p>
+        <p class="livro-texto"><span class="bold">Idioma: </span>${idiomaLivro}</p>
+        <p class="livro-texto"><span class="bold">Publicado em: </span>${dataPublicacao}</p>
+        <p class="livro-texto"><span class="bold">Editora: </span>${editoraLivro}</p>
+    </div>
+
+    <div>
+        <a href="#" class="botao-saiba-mais">Saiba mais </a>
+    </div>
+
+</div>
+</li>`;
+
+    return template;
+}
